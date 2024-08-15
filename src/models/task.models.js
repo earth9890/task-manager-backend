05b304391063b0066ae7af24f53b6b54ddcb6ac1
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-08-15 22:18:39 +0530

    added Task Schema

---
+++ src/models/task.models.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema({
+	title: { type: String, required: true },
+	description: { type: String },
+	completed: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
